cd83a6237dc86714f7c00f891a6bb5b20ac3c403
Durga Prasad <bdurga.24bcs10012@sst.scaler.com>
2026-06-04 16:47:07 +0530

    fix: remove outputFileTracingRoot to resolve vercel path resolution bug

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['@prisma/client', 'prisma'],
-  outputFileTracingRoot: path.join(__dirname, '../'),
   images: {
     domains: ['avatars.githubusercontent.com', 'github.com'],
   },
